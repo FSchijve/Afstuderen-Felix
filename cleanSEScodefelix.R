1a323f0d9c8a510eb28f4e61bd3e4a14d3056c31
FSchijve <58231888+FSchijve@users.noreply.github.com>
2022-03-22 16:34:35 +0100

    Add files via upload

--- cleanSEScodefelix.R
+++ cleanSEScodefelix.R
@@ -1,4 +1,5 @@
 library(ggplot2)
+library(MLmetrics)
 
 #import data
 data <- read.csv(file="C:\\Users\\s166646\\Downloads\\prevalentie_data.csv",sep = ';')
@@ -15,52 +16,84 @@ plot(data$day, data$prevalence, ylim=c(-0.025, 0.20), type='l', main='Daily Prev
 
 #perform SES using pre-existing library to check results later
 ses.prevalence <- ses(data$prevalence, alpha=.2, h=50)
-plot(x=data$day, y=ses.prevalence,ylim=c(-0.025, 0.20), main="SES Prevalence using pre-existing library", xlab="date", ylab="prevalence")
+plot(ses.prevalence,ylim=c(-0.025, 0.20), main="SES Prevalence using pre-existing library", xlab="date", ylab="prevalence")
 summary(ses.prevalence)
 
-#SES in component form: y[t+1] = a*y[t] + (1-a)*y[t-1]
-componentSESprevalence <- function(a) {
-  predictedvals = c(data$prevalence[1])
+#perform SES using component form
+componentSES <- function(alpha) {
+  #Set y0 = 0
+  predictedvals = c(0)
   for (i in 1:length(data$prevalence)){
-    value = a*data$prevalence[i] + ((1-a)*data$prevalence[i-1])
-    predictedvals <- append(predictedvals, value)
+    et = data$prevalence[i] - predictedvals[i-1]
+    predictedval = predictedvals[i-1] + alpha*et
+    predictedvals <- append(predictedvals, predictedval)
   }
+  #Weird interaction, but an extra 0 at the beginning of the vector is needed to produce correct results
+  predictedvals <- c(rep(0,1),predictedvals)
   return(predictedvals)
 }
-componentSESvals <- componentSESprevalence(0.2)
 
-componentSESprevalence2 <-function(a){
-  
-}
-
-#plot the SES component form results
-plot(x=data$day,y=componentSESvals,type='l',ylim=c(-0.025, 0.20), main="SES prevalence using component form", xlab="date", ylab="prevalence")
-
-#Weighted average SES
-waSES <- function(N) {
-  #work with copies as the formula is gonna adjust some data
+#SES in weighted average form
+#Initially used to verify results, not used anymore as the code is inefficient and the component form is easier to expand on
+waSESprevalence <- function(a) {
   prevalencecopy = data$prevalence
   daycopy = data$day
-  
-  #Define prediction and sliding window sizes
-  a = 2/(N+1)
+  #a = 2/(N+1)
   predictedvals = c(0) #put l0 to zero
-  nrofiters = length(data$prevalence)+50 #predict 50 days into the future
+  nrofiters = length(data$daily_tot) #predict 50 days into the future
   
-  #we need to predict every Y value from t=1 to t=507
-  for (T in 1:nrofiters){
+  #Loop over all T's
+  for (val in 1:nrofiters){
+    allterms = 0
+    j = 0
     
-    #Sum from j=0 to j=T-1
-    j <- (0:(T-1)) 
-    allterms = sum(a*((1-a)^j)*prevalencecopy[T-j])
+    #Sum from j=0 to T-1
+    for (i in 0:(val-1)){
+      term = a*((1-a)^j)*prevalencecopy[val-j]
+      allterms = allterms + term
+      j = j + 1
+    } 
     predictedvals = append(predictedvals, allterms)
     
     #If there is no existing datapoint, add the prediction to the list
-    if (T >= length(data[2])){
+    if (val >= length(data[2])){
       prevalencecopy = append(prevalencecopy, allterms)
+      
     }
   }
+  
   return(predictedvals)
 }
 
-waSESvals <- waSES(50)
+#Use both methods and compare results
+waSESvals <- waSESprevalence(0.5)
+componentSESvals <- componentSES(0.5)
+
+#plot results
+plot(predictedvals,type='l',ylim=c(-0.025, 0.60), main="SES prevalence using component form", xlab="day", ylab="prevalence")
+plot(waSESvals, ylim=c(-0.025, 0.60), type='l', main="SES prevalence using weighted average form", xlab="day", ylab="prevalence")
+
+#Function to determine optimal alpha from a list of alphas
+determineAlpha <- function(alphas){
+  allMSEs <- c()
+  for (alpha in alphas){
+    
+    predictions = componentSES(alpha)
+    TotalMSE = MSE(predictions, data$prevalence)
+    
+    allMSEs <- append(allMSEs, TotalMSE)
+  }
+  plot(alphas,allMSEs, main='MSE per alpha value')
+  
+  lowestMSE = min(allMSEs)
+  lowestMSEindex = which.min(allMSEs)
+  lowestalpha = alphas[lowestMSEindex]
+  
+  print('lowest MSE:')
+  print(lowestMSE)
+  print('lowest alpha:')
+  print(lowestalpha)
+ 
+}
+
+determineAlpha(seq(0.01, 1, by=0.01))
